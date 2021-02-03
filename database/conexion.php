ea68645a06355aa4f5537bc5bccf06b0a120e1fb
Genaro <laureanomg2@gmail.com>
2021-02-03 11:18:59 -0600

    bd

--- database/conexion.php
+++ database/conexion.php
@@ -1,19 +1,8 @@
 <?php
-// $dbhost = 'bqtleac8ctwnn8ij3ly1-mysql.services.clever-cloud.com';
-// $dbuser = 'ua2gga6vun9cg8fi';
-// $dbpass = '1GnNQztAHZkk1xnUGRtV';#Cambiar dependiendo de si su bd tiene password
-// $dbdata = 'bqtleac8ctwnn8ij3ly1'; #nombre de base datos
-// $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbdata);
-// $conn->set_charset("utf8");
-// if ($conn->connect_error){
-//     die('Error de conexion: proceso cancelado');
-// } else{
-//     #echo "Conexion exitosa";
-// }
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = '12345678';#Cambiar dependiendo de si su bd tiene password
-$dbdata = 'ventas'; #nombre de base datos
+$dbhost = 'bqtleac8ctwnn8ij3ly1-mysql.services.clever-cloud.com';
+$dbuser = 'ua2gga6vun9cg8fi';
+$dbpass = '1GnNQztAHZkk1xnUGRtV';#Cambiar dependiendo de si su bd tiene password
+$dbdata = 'bqtleac8ctwnn8ij3ly1'; #nombre de base datos
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbdata);
 $conn->set_charset("utf8");
 if ($conn->connect_error){
@@ -21,3 +10,14 @@ if ($conn->connect_error){
 } else{
     #echo "Conexion exitosa";
 }
+// $dbhost = 'localhost';
+// $dbuser = 'root';
+// $dbpass = '12345678';#Cambiar dependiendo de si su bd tiene password
+// $dbdata = 'ventas'; #nombre de base datos
+// $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbdata);
+// $conn->set_charset("utf8");
+// if ($conn->connect_error){
+//     die('Error de conexion: proceso cancelado');
+// } else{
+//     #echo "Conexion exitosa";
+// }
